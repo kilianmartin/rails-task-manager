31d7247c255172d47198b39da11a1372e38a0f11
Kilian Martin <kilian.martin@me.com>
2017-08-15 11:03:17 +0200

    created DB

---
+++ db/migrate/20170815090115_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.string :description
+      t.boolean :completed
+      t.integer :priority
+
+      t.timestamps
+    end
+  end
+end
