4000d35a3c3b4b39c40f5af55081929f610a43ec
Kilian Martin <kilian.martin@me.com>
2017-08-15 15:39:42 +0200

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(name: "Clean kitchen", description: "very painful")
+Task.create(name: "Swipe floor", description: "also very painful")
+Task.create(name: "Do laundry", description: "really don't wanna do it...")
+Task.create(name: "Do groceries", description: "so expensive...")
